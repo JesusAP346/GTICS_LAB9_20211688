d9b1f9f558aba91a0df0afe100dc3c4f1862074f
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-11-10 23:21:24 -0500

    a

---
+++ src/main/java/org/example/gtics_lab9_20211688/GticsLab920211688Application.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab9_20211688;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab920211688Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab920211688Application.class, args);
+    }
+
+}
